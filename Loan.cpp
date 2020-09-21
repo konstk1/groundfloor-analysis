cb723e9b5b3c11abedbd035655f81629405c69c3
Konstantin Klitenik <kostyan5@gmail.com>
2020-09-21 17:11:56 -0400

    Move some function defs out of Loan header

--- Loan.cpp
+++ Loan.cpp
@@ -2,8 +2,24 @@
 
 #include "Loan.hpp"
 
-std::ostream &operator<<(std::ostream &os, Date const &d) { 
-    return os << std::setw(2) << std::setfill('0') << d.mon << "/" 
-              << std::setw(2) << std::setfill('0') << d.day << "/" 
-               << d.year;
+void Loan::Print() const {
+    std::cout << "Loan: " << title << std::endl;
+    std::cout << address.street << ", " << address.city << ", " << address.state << ", " << address.zipcode << std::endl;
+    std::cout << "Grade: " << grade << ", Term: " << term << ", Rate: " << rate << "%, ARV: " << arvPct << "%" << std::endl;
+    std::cout << "Purchased: " << purchasedDate.AsString() << ", Maturity: " << maturityDate.AsString() << ", Repaid: " << repaidDate.AsString() << std::endl;
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "Amount: $" << purchasedAmount << ", Repaid: $" << repaidAmount
+              << " (" << ReturnPct() << "%, Annual: " << ReturnPctAnnualized() << "%)" << std::endl;
 }
+
+std::vector<Transaction> Loan::GetTransactions() const {
+    std::vector<Transaction> transactions;
+
+    transactions.push_back(Transaction{id, title, purchasedDate, 0 - purchasedAmount});  // purchase is negative transaction
+
+    if (repaidDate.IsValid()) {
+        transactions.push_back(Transaction{id, title, repaidDate, repaidAmount});  // repaid is positive transaction
+    }
+
+    return transactions;
+};
--- Loan.hpp
+++ Loan.hpp
@@ -66,7 +66,11 @@ struct Date {
     int year = 0;   // year
 };
 
-std::ostream &operator<<(std::ostream &os, Date const &d);
+inline std::ostream &operator<<(std::ostream &os, Date const &d) { 
+    return os << std::setw(2) << std::setfill('0') << d.mon << "/" 
+              << std::setw(2) << std::setfill('0') << d.day << "/" 
+               << d.year;
+}
 
 inline bool operator==(const Date& lhs, const Date& rhs) { 
     return (lhs.year == rhs.year) && (lhs.mon == rhs.mon) && (lhs.day == rhs.day);
@@ -169,31 +173,8 @@ struct Loan {
         return ReturnPct() * (365.0 / DurationDays());
     }
 
-    void Print() const {
-        std::cout << "Loan: " << title << std::endl;
-        std::cout << address.street << ", " << address.city << ", " << address.state << ", " << address.zipcode << std::endl;
-        std::cout << "Grade: " << grade << ", Term: " << term << ", Rate: " << rate << "%, ARV: " << arvPct << "%" << std::endl;
-        std::cout << "Purchased: " << purchasedDate.AsString() << ", Maturity: " << maturityDate.AsString() << ", Repaid: " << repaidDate.AsString() << std::endl;
-        std::cout << std::fixed << std::setprecision(2);
-        std::cout << "Amount: $" << purchasedAmount << ", Repaid: $" << repaidAmount 
-                  << " (" << ReturnPct() << "%, Annual: " << ReturnPctAnnualized() << "%)" <<  std::endl;
-    }
-
-    std::vector<Loan> CalendarYearSplit() {
-        return std::vector<Loan>{};
-    };
-
-    std::vector<Transaction> GetTransactions() const {
-        std::vector<Transaction> transactions;
-
-        transactions.push_back(Transaction{id, title, purchasedDate, 0-purchasedAmount}); // purchase is negative transaction
-
-        if (repaidDate.IsValid()) {
-            transactions.push_back(Transaction{id, title, repaidDate, repaidAmount});       // repaid is positive transaction
-        }
-        
-        return transactions;
-    };
+    void Print() const;
+    std::vector<Transaction> GetTransactions() const;
 };
 
 #endif
